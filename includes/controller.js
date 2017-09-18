abbe60727cb7f801c892497c6ed6de30bcdcb9f1
Michi <michi@bossdonkey.com.au>
2017-09-18 01:05:34 +0000

    add controller and updates to index

---
+++ includes/controller.js
@@ -0,0 +1,21 @@
+
+var app = angular.module("toDoList", []); 
+app.controller("myCtrl", function($scope) {
+    $scope.item = ["Today I must"];
+    $scope.addItem = function () {
+        $scope.errortext = "";
+        if (!$scope.addMe) {return;}
+        if ($scope.item.indexOf($scope.addMe) == -1) {
+            $scope.item.push($scope.addMe);
+        } else {
+            $scope.errortext = "Already in list.";
+        }
+    }
+    $scope.removeItem = function (x) {
+        $scope.errortext = "";    
+        $scope.products.splice(x, 1);
+    }
+
+    $scope.date = new Date();
+    
+});
